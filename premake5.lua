ca4534eb71e76e1153a55fb292248d1614b145d3
MchengWang <2734190606@qq.com>
2024-12-24 16:44:36 +0800

    添加 premake 构建项目

---
+++ premake5.lua
@@ -0,0 +1,110 @@
+workspace "Orange"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Orange"
+	location "Orange"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outdir .. "/%{prj.name}")
+	objdir ("bin-obj/" .. outdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+filter "system:windows"
+	cppdialect "C++17"
+	staticruntime "On"
+	systemversion "latest"
+
+	defines
+	{
+		"OG_PLATFORM_WINDOWS",
+		"OG_BUILD_DLL"
+	}
+
+	postbuildcommands
+	{
+		("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outdir .. "/Sandbox")
+	}
+
+	filter "configurations:Debug"
+		defines "OG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "OG_DEBUG"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OG_DEBUG"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outdir .. "/%{prj.name}")
+	objdir ("bin-obj/" .. outdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Orange/vendor/spdlog/include",
+		"Orange/src"
+	}
+
+	links
+	{
+		"Orange"
+	}
+
+filter "system:windows"
+	cppdialect "C++17"
+	staticruntime "On"
+	systemversion "latest"
+
+	defines
+	{
+		"OG_PLATFORM_WINDOWS"
+	}
+
+	postbuildcommands
+	{
+		("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outdir .. "/Sandbox")
+	}
+
+	filter "configurations:Debug"
+		defines "OG_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "OG_DEBUG"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OG_DEBUG"
+		optimize "On"
