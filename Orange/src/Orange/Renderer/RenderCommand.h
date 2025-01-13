bc65fb7c89c3764fc2d7a9d6a6ac3e02f66231b2
MchengWang <2734190606@qq.com>
2025-01-13 09:40:57 +0800

    添加渲染线条

--- Orange/src/Orange/Renderer/RenderCommand.h
+++ Orange/src/Orange/Renderer/RenderCommand.h
@@ -28,9 +28,19 @@ namespace Orange
 			o_RendererAPI->Clear();
 		}
 
-		static void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t count = 0)
+		static void DrawIndexed(const Ref<VertexArray>& vertexArray, uint32_t indexCount = 0)
 		{
-			o_RendererAPI->DrawIndexed(vertexArray, count);
+			o_RendererAPI->DrawIndexed(vertexArray, indexCount);
+		}
+
+		static void DrawLines(const Ref<VertexArray>& vertexArray, uint32_t vertexCount)
+		{
+			o_RendererAPI->DrawLines(vertexArray, vertexCount);
+		}
+
+		static void SetLineWidth(float width)
+		{
+			o_RendererAPI->SetLineWidth(width);
 		}
 
 	private:
