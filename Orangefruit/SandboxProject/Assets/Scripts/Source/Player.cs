c8cd7c8fbe6d2ec23c9e167b51c43cea74560d26
MchengWang <2734190606@qq.com>
2025-01-15 11:35:28 +0800

    维护

--- Orangefruit/SandboxProject/Assets/Scripts/Source/Player.cs
+++ Orangefruit/SandboxProject/Assets/Scripts/Source/Player.cs
@@ -28,7 +28,7 @@ namespace Sandbox
 			Vector3 velocity = Vector3.Zero;
 
 			if (Input.IsKeyDown(KeyCode.W))
-				velocity.Y = 1.0f;
+				velocity.Y = 1.0f;	
 			else if (Input.IsKeyDown(KeyCode.S))
 				velocity.Y = -1.0f;
 			if (Input.IsKeyDown(KeyCode.A))
